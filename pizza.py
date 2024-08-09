47e735457c0f7554d65693b3e243bb05cd969cd7
MGeary313 <matthew.geary313@gmail.com>
2024-08-09 16:07:04 -0400

    pizza

---
+++ pizza.py
@@ -0,0 +1,43 @@
+print ("Welcome to pizza builder")
+
+size = input("What size pizza do you want? S, M, or L:\n").title()
+pepperoni = input("Do you want pepperoni? Y or N:\n").title()
+extraCheese = input("Do you want extra cheese? Y or N:\n").title()
+
+cost = 0
+i = 0
+
+small = 15
+medium = 20
+large = 25
+smallPepperoni = 2
+mediumLargePepperoni = 3
+cheese = 1
+
+
+while i < 1:
+    if size == "S":
+        cost = cost + small
+    elif size == "M":
+        cost = cost + medium
+    elif size == "L":
+        cost = cost + large
+    else:
+        print("Please choose a size S, M, or L")
+
+    if size == "S" and pepperoni == "Y":
+        cost = cost + smallPepperoni
+    elif size == "S" and pepperoni == "N":
+        cost = cost
+    elif size == "M" or size == "L" and pepperoni == "Y":
+        cost = cost + mediumLargePepperoni
+    else:
+        cost = cost
+
+    if extraCheese == "Y":
+        cost = cost + cheese
+    else:
+        cost = cost
+    i+=1
+
+print(f"Your total bill is: ${cost}")
