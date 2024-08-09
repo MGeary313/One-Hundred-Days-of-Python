910e3c982573b987ad9fc93b6fa497653621a8dc
MGeary313 <matthew.geary313@gmail.com>
2024-08-09 14:57:09 -0400

    day1

---
+++ bandNameGen.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator.")
+
+
+cityName = input("What is the name of the city you grew up in?\n")
+petName = input("What is your pets name?\n")
+
+print ("Your band name could be", cityName.title(), petName.title())
