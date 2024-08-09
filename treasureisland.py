073779485b0f7d5d2fced5267efe1d611b38d412
MGeary313 <matthew.geary313@gmail.com>
2024-08-09 16:40:42 -0400

    treasureisland

---
+++ treasureisland.py
@@ -0,0 +1,31 @@
+print("Welcome to Treasure Island\n")
+print("Your mission is to find the treasure\n")
+
+while True:
+    crossroad = input("You're at a cross road. Where do you want to go? Left or Right?\n").title()
+    if crossroad == "Right":
+        print("You fall into a hole. Game over.")
+        break
+    elif crossroad == "Left":
+        river = input("You come to a raging river. Do you want to attempt to swim across? Yes or No:\n").title()
+        if river == "Yes":
+            print("You drown attempting to cross the river. Game over.")
+            break
+        elif river == "No":
+            door = input("A wagon comes along and offers you a ride. Do you get in through the Red, Blue, or Yellow door?\n").title()
+
+            if door == "Red":
+                print("The door was booby trapped. Game over.")
+                break
+            elif door == "Blue":
+                print("The horse panics and kicks you, breaking your legs. Game over.")
+                break
+            elif door == "Yellow":
+                print("You win! You have made it to the end of the game.")
+                break
+            else:
+                print("Please choose Red, Blue, or Yellow\n")
+        else:
+            print("Please choose Yes or No.")
+    else:
+        print("Please choose Left or Right.\n")
