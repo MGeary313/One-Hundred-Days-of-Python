cfd18c82af35a2d61a247e90c262df59cc7d4ee3
MGeary313 <matthew.geary313@gmail.com>
2024-08-09 16:11:57 -0400

    pizza

--- pizza.py
+++ pizza.py
@@ -5,7 +5,6 @@ pepperoni = input("Do you want pepperoni? Y or N:\n").title()
 extraCheese = input("Do you want extra cheese? Y or N:\n").title()
 
 cost = 0
-i = 0
 
 small = 15
 medium = 20
@@ -15,29 +14,28 @@ mediumLargePepperoni = 3
 cheese = 1
 
 
-while i < 1:
-    if size == "S":
-        cost = cost + small
-    elif size == "M":
-        cost = cost + medium
-    elif size == "L":
-        cost = cost + large
-    else:
-        print("Please choose a size S, M, or L")
 
-    if size == "S" and pepperoni == "Y":
-        cost = cost + smallPepperoni
-    elif size == "S" and pepperoni == "N":
-        cost = cost
-    elif size == "M" or size == "L" and pepperoni == "Y":
-        cost = cost + mediumLargePepperoni
-    else:
-        cost = cost
+if size == "S":
+    cost = cost + small
+elif size == "M":
+    cost = cost + medium
+elif size == "L":
+    cost = cost + large
+elif size != "S" and size != "M" and size != "L":
+    print("Please choose a size S, M, or L")
 
-    if extraCheese == "Y":
-        cost = cost + cheese
-    else:
-        cost = cost
-    i+=1
+if size == "S" and pepperoni == "Y":
+    cost = cost + smallPepperoni
+elif size == "S" and pepperoni == "N":
+    cost = cost
+elif size == "M" or size == "L" and pepperoni == "Y":
+    cost = cost + mediumLargePepperoni
+else:
+    cost = cost
+
+if extraCheese == "Y":
+    cost = cost + cheese
+else:
+    cost = cost
 
 print(f"Your total bill is: ${cost}")
