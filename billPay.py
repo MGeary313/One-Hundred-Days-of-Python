31a48e6c88539a983b829aab75b4cd6b6ff4dede
mgeary313 <matthew.geary313@gmail.com>
2024-08-10 10:55:36 -0400

    billpay

---
+++ billPay.py
@@ -0,0 +1,9 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emmanuel"]
+
+randNum = random.randint(0, 4)
+
+billPay = friends[randNum]
+
+print(f"{billPay} is paying for dinner tonight.")
